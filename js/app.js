79af031778625b61d7095151e782d9e09c8fcd80
Pallas <pallasravae@yahoo.com>
2015-08-05 11:18:03 -0600

    jquery

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 $('.ryu').mouseenter(function() {
 	$('.ryu-still').hide();
 	$('.ryu-ready').show();
-	});
+	})
 	.mouseleave(function() {
 	$('.ryu-ready').hide();
 	$('.ryu-still').show();	
