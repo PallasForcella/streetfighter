7f56cd1d98780cec3e48e9e1ea5baf5c7eed36cb
Pallas <pallasravae@yahoo.com>
2015-08-05 15:32:04 -0600

    keydown

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ function playHadouken () {
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+$(document).keydown(function(e){
+    if (e.which == 88){
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').show();
+    }
+});
 $('.ryu').mouseenter(function() {
 	$('.ryu-still').hide();
 	$('.ryu-ready').show();
@@ -29,13 +37,5 @@ $('.ryu').mouseenter(function() {
 	$('.ryu-throwing').hide();
     $('.ryu-ready').show();
 	})
-	.on('keydown', function(e){
-    console.log(e);
-    if (e.which == 88){
-        $('.ryu-ready').hide();
-        $('.ryu-still').hide();
-        $('.ryu-throwing').hide();
-        $('.ryu-cool').show();
-    }
-	});
+	
 });
