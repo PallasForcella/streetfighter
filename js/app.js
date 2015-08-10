41201f219ed4b15c9715d0c3e4f9237858fca9be
Pallas <pallasravae@yahoo.com>
2015-08-10 13:22:34 -0600

    nggyu

--- js/app.js
+++ js/app.js
@@ -1,55 +1,60 @@
 $(document).ready(function () {
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
-function playnggyu () {
-  $('#nggyu')[0].volume = 0.5;
-  $('#nggyu')[0].load();
-  $('#nggyu')[0].play();
-}
-$(document).keydown(function(e){
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+  var nggyusong = false;
+  function playnggyu () {
+    nggyusong = !nggyusong;
+    if (nggyusong) {
+    $('#nggyu')[0].volume = 0.5;
+    //$('#nggyu')[0].load();
+    $('#nggyu')[0].play();
+  }
+  }
+  $(document).keydown(function(e){
     if (e.which == 88){
-    	playnggyu();
-        $('.ryu-ready').hide();
-        $('.ryu-still').hide();
-        $('.ryu-throwing').hide();
-        $('.ryu-cool').show();
+      playnggyu();
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
     }
-})
-$(document).keyup(function(e){
+  });
+  $(document).keyup(function(e){
     if (e.which == 88){
-        $('.ryu-ready').hide();
-        $('.ryu-still').show();
-        $('.ryu-throwing').hide();
-        $('.ryu-cool').hide();
+      $('#nggyu')[0].pause();
+      $('#nggyu')[0].load();
+      $('.ryu-ready').hide();
+      $('.ryu-still').show();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').hide();
     }
-});
-$('.ryu').mouseenter(function() {
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-	$('.ryu-ready').hide();
-	$('.ryu-still').show();
-	})
-	.mousedown(function() {
-	playHadouken();
- 	$('.ryu-ready').hide();
+  });
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+    .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+    .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
-    	.animate(
-    	{'left':'1020px'},
-    	500,
-    	function(){
-    		$(this).hide();
-    		$(this).css('left','600px');
-    		})
-	})
-	.mouseup(function(){
-	$('.ryu-throwing').hide();
+      .animate(
+      {'left':'1020px'},
+      500,
+      function(){
+        $(this).hide();
+        $(this).css('left','600px');
+      });
+  })
+    .mouseup(function(){
+    $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-	})
-	
+  });
 });
